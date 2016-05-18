6e08c7ea96c8c23c05c371d15c94149b3d58dcd9
Rizda Dwi Prasetya <rizda.prasetya@veritrans.co.id>
2016-05-18 10:52:20 +0700

    update cron job

--- cron_cancel_vt.php
+++ cron_cancel_vt.php
@@ -2,8 +2,18 @@
 include(dirname(__FILE__).'/../../config/config.inc.php');
 include(dirname(__FILE__).'/../../init.php');
 
-$sql = 'SELECT id_order FROM '._DB_PREFIX_.'orders WHERE current_state = (SELECT id_order_state FROM '._DB_PREFIX_.'order_state WHERE module_name = "veritranspay") AND date_add < TIMESTAMPADD(MINUTE,(12*60)-15,now())';
-
+$sql = "SELECT * 
+		FROM (
+				SELECT 	a.id_order
+						,a.date_add
+						,(SELECT id_order_state FROM "._DB_PREFIX_."order_history WHERE id_order=a.id_order ORDER BY date_add DESC LIMIT 0,1) AS current_state 
+				FROM "._DB_PREFIX_."orders a
+				WHERE 
+					module = 'veritranspay'
+			) b
+		WHERE date_add < TIMESTAMPADD(MINUTE,(12*60)-15,now())
+			AND current_state=(SELECT DISTINCT id_order_state FROM "._DB_PREFIX_."order_state_lang WHERE name='Awaiting Veritrans payment')
+		";
 
 $results = Db::getInstance()->ExecuteS($sql);
 if (!$results)
